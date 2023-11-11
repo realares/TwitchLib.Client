cfce17d826f4253fb4db9e264cbf107913101d7a
realares <github_KJN572a@realares.de>
2023-11-11 21:05:06 +0100

    update auf .net8

--- TwitchLib.Client.RA/TwitchClientExtensions.cs
+++ TwitchLib.Client.RA/TwitchClientExtensions.cs
@@ -13,6 +13,7 @@ namespace TwitchLib.Client.RA
         /// <inheritdoc />
         public static Task SendReplyAsync(this TwitchClient client, ChatMessage replyToMsg, string message, bool dryRun = false)
         {
+            if (string.IsNullOrEmpty(message)) return Task.CompletedTask;
             return client.SendReplyAsync(client.GetJoinedChannel(replyToMsg.Channel), replyToMsg.Id, message, dryRun);
         }
 
